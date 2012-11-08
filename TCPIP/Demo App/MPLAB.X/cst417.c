b2b6de57eb59f15fddd71007a68fba28056c7249
MechAlucard <tyler.j.martin@gmail.com>
2012-11-08 14:43:04 -0800

    Mac address hardcoded to broadcast in CST417SendEth()

--- TCPIP/Demo App/MPLAB.X/cst417.c
+++ TCPIP/Demo App/MPLAB.X/cst417.c
@@ -1,21 +1,14 @@
 #include "cst417.h"
 #include "TCPIP Stack/TCPIP.h"
+#define TYLER_CODE 0x65
 static BOOL CST417SendEth(CST417_PACKET * packet)
 {
     while(!MACIsTxReady());
 	MACSetWritePtr(BASE_TX_ADDR);
         //packet->Operation;
-
-    //memcpy(&packet->SenderMACAddr, (void*)&AppConfig.MyMACAddr, sizeof(packet->SenderMACAddr));
-#ifdef STACK_USE_ZEROCONF_LINK_LOCAL
-    //packet->SenderIPAddr  = AppConfig.MyIPAddr; /* Removed for ZCLL, SenderIPAddr should be filled in */
-#else
-   // packet->SenderIPAddr  = AppConfig.MyIPAddr;
-#endif
-
-    //SwapARPPacket(packet);
-
-    //MACPutHeader(&packet->TargetMACAddr, MAC_ARP, sizeof(*packet));
+        //set to broadcast
+        MAC_ADDR  TargetMACAddr = {'0xff','0xff','0xff','0xff','0xff','0xff','0xff'};
+    CST417PutHeader(&TargetMACAddr, MAC_ARP, sizeof(*packet));
     MACPutArray((BYTE*)packet, sizeof(*packet));
     MACFlush();
 
