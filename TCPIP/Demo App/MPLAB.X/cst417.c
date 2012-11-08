3d9bc197b3a399f9110f015328464f81ddab4f55
MechAlucard <tyler.j.martin@gmail.com>
2012-11-08 14:08:20 -0800

    Added CST417PutHeader() minor changes to others
    changed CST417SendEth(), still inprogress

--- TCPIP/Demo App/MPLAB.X/cst417.c
+++ TCPIP/Demo App/MPLAB.X/cst417.c
@@ -1,1 +1,23 @@
+#include "cst417.h"
+#include "TCPIP Stack/TCPIP.h"
+static BOOL CST417SendEth(CST417_PACKET * packet)
+{
+    while(!MACIsTxReady());
+	MACSetWritePtr(BASE_TX_ADDR);
+        //packet->Operation;
 
+    //memcpy(&packet->SenderMACAddr, (void*)&AppConfig.MyMACAddr, sizeof(packet->SenderMACAddr));
+#ifdef STACK_USE_ZEROCONF_LINK_LOCAL
+    //packet->SenderIPAddr  = AppConfig.MyIPAddr; /* Removed for ZCLL, SenderIPAddr should be filled in */
+#else
+   // packet->SenderIPAddr  = AppConfig.MyIPAddr;
+#endif
+
+    //SwapARPPacket(packet);
+
+    //MACPutHeader(&packet->TargetMACAddr, MAC_ARP, sizeof(*packet));
+    MACPutArray((BYTE*)packet, sizeof(*packet));
+    MACFlush();
+
+	return TRUE;
+}
--- TCPIP/Demo App/MPLAB.X/cst417.h
+++ TCPIP/Demo App/MPLAB.X/cst417.h
@@ -4,17 +4,33 @@
  *
  * Created on November 8, 2012, 12:43 PM
  */
-
+#include "GenericTypeDefs.h"
 #ifndef CST417_H
 #define	CST417_H
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
+// For a CST417_PACKET, the first two bytes are DST and SRC Node_IDs. They
+// are followed by the Operation and the Data fields.
+//
+// Operation field (2 bytes) is defined as follows:
+// 0 - Request MAC address from Node_ID (Sent to ff-ff-ff-ff-ff-ff)
+// 1 - Reply from Node_ID with MAC Address (Sent to re-qu-es-te-rM-AC)
+// 2 - Control LED at Node_ID (Sent to co-nt-ro-le-dM-AC)
+//
+// The 10 bytes of data will be populated based on Operation:
+// 0 - 0 bytes = No data
+// 1 - 6 bytes = Src MAC Address
+// 2 - 3 bytes = LED control value: 1 -> ON, 0 -> OFF
+typedef struct __attribute__((aligned(2), packed))
+{
+    WORD        Operation;   // operation code
+    BYTE        Data[10];     // holds 10 bytes of data dependant on the opcode
 
+} CST417_PACKET;
 
-
-
+static BOOL CST417SendEth(CST417_PACKET * packet);
 #ifdef	__cplusplus
 }
 #endif
